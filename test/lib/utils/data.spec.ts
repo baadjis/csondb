f5f0792395efca745f3c56e83a70d7343a9a0f92
baadjis <baadjisidy@gmail.com>
2024-09-11 19:39:21 +0200

    updated tests

--- test/lib/utils/data.spec.ts
+++ test/lib/utils/data.spec.ts
@@ -1,4 +1,4 @@
-import { applyFilter, applyOperator, isKeyWord } from '../../../src/lib/utils/data';
+import { applyFilter, applyOperator, applyOptions, findIndexWithCondition, findManyWithConditon, isKeyWord } from '../../../src/lib/utils/data';
 import {expect} from 'chai';
 
 describe('is keyword test',()=>{
@@ -41,7 +41,7 @@ describe('apply filter test',()=>{
     const data={firstname:'cnd',lastname:'baadji',address:{
         rue:'18 rue liné',codepostal:'75005'
     }}
-    it('shoould return true',()=>{
+    it('should return true',()=>{
          const val=applyFilter(data,{$or:[{firstname:'cnd'},{lastname:'baadjis'}]});
          expect(val).to.be.true
         }
@@ -52,4 +52,114 @@ describe('apply filter test',()=>{
         expect(val).to.be.false
        }
    )
+
+   it('shoould return true',()=>{
+    const val=applyFilter(data,{address:{codepostal:'75005'}});
+    expect(val).to.be.true
+   }
+)
+it('should return error',()=>{
+    const val=applyFilter(data,{firstname:{$isin: 'cnd'}});
+    expect(val).to.be.throw
+   }
+)
+
+})
+
+describe('find index with condition test',()=>{
+    const data=[
+        {firstname:'cnd',
+            lastname:'baadji',
+            address:{
+            rue:'18 rue liné',
+            codepostal:'75005'
+          }
+        }
+        ,
+        {
+          firstname:'cnd1',
+          lastname:'baadjis',
+            address:{
+            rue:'18 rue liné',
+            codepostal:'75005'
+           }
+        }
+    ]
+    it('should return -1',()=>{
+        const val=findIndexWithCondition(data,{firstname:'cnd2'})
+        expect(val).to.be.equal(-1)
+    })
+    it('should return 1',()=>{
+        const val=findIndexWithCondition(data,{firstname:'cnd1'})
+        expect(val).to.be.equal(1)
+    })
+
+    it('should return -1',()=>{
+        const val=findIndexWithCondition(data,{firstname:'cnd1',lastname:'baadjic'})
+        expect(val).to.be.equal(-1)
+    })
+
+    it('should return 1',()=>{
+        const val=findIndexWithCondition(data,{ $or:[{firstname:'cnd1'},{lastname:'baadjic'}]})
+        expect(val).to.be.equal(1)
+    })
+})
+describe('find many with condition',()=>{
+    const data=[
+        {firstname:'cnd',
+            lastname:'baadji',
+            address:{
+            rue:'18 rue liné',
+            codepostal:'75005'
+          }
+        }
+        ,
+        {
+          firstname:'cnd1',
+          lastname:'baadjis',
+            address:{
+            rue:'18 rue liné',
+            codepostal:'75005'
+           }
+        }
+    ]
+    it('should return true',()=>{
+        const val =findManyWithConditon(data,{ $or:[{firstname:'cnd1'},{lastname:'baadjic'}]});
+        expect(Array.isArray(val)).to.be.true
+    })
+})
+
+describe('apply options test',()=>{
+    const data=[
+        {firstname:'cnd',
+            lastname:'baadji',
+            address:{
+            rue:'18 rue liné',
+            codepostal:'75005'
+          }
+        }
+        ,
+        {
+          firstname:'cnd1',
+          lastname:'baadjis',
+            address:{
+            rue:'18 rue liné',
+            codepostal:'75005'
+           }
+        }
+    ]
+    it('should return 1',()=>{
+        const val= (applyOptions(data,{limit:1}) as any[])
+        expect(val.length).to.be.equal(1)
+    })
+    it('should return true',()=>{
+        applyOptions(data,{sort:{firstname:1}})
+    
+        const result=data[1]['firstname']>data[0]['firstname']
+        expect(result).to.be.true
+    })
+    it('should return 1',()=>{
+        const val= (applyOptions(data,{skip:1}) as any[])
+        expect(val.length).to.be.equal(1)
+    })
 })
---
+++ test/lib/utils/files.spec.ts
@@ -0,0 +1,72 @@
+import {expect} from 'chai';
+import { readJson, updateJson, updateJsonWithArray, writeJson } from '../../../src/lib/utils/files';
+import fs from 'fs'
+describe('readJson test',()=>{
+
+    it('should be empty',()=>{
+        if(fs.existsSync(__dirname+'/test.json')){
+            fs.unlinkSync(__dirname+'/test.json')
+        }
+        const val = readJson(__dirname+'/test.json');
+        expect(val).to.be.empty
+    })
+    it('should return true',()=>{
+        
+        const result=readJson(__dirname+'/test1.json') as any[]
+        expect(result.length>=1).to.be.true
+    })
+})
+
+describe('writeJson test',()=>{
+    const data={firstname:'cnd',lastname:'baadji',address:{
+        rue:'18 rue liné',codepostal:'75005'
+    }}
+    it('should return true',()=>{
+        const datar=writeJson(data, __dirname+'/test.json')
+        const result=fs.existsSync(__dirname+'/test.json')
+        expect(result).to.be.true
+    })
+    
+})
+
+describe('updateJson test',()=>{
+    const data={firstname:'cnd',lastname:'baadji',address:{
+        rue:'18 rue liné',codepostal:'75005'
+    }}
+    const data1={firstname:'cnd1',lastname:'baadjis',address:{
+        rue:'18 rue liné',codepostal:'75005'
+    }}
+    
+  it('should return true',()=>{
+      const datar=updateJson(data,__dirname+'/test2.json',true)
+      const res=(Object.keys(datar).includes('id') &&  Object.keys(datar).includes('createdAt'))
+      const res1= readJson(__dirname+'/test2.json') as any[];
+      const datar1=updateJson(data1,__dirname+'/test2.json',true)
+      //after update
+      const res2= readJson(__dirname+'/test2.json')as any[];
+      //compare legth after update
+      expect(res&&(res2.length>res1.length)).to.be.true
+  })
+
+})
+
+describe('updateJsonWithArray test',()=>{
+    const data=[{firstname:'cnd',lastname:'baadji',address:{
+        rue:'18 rue liné',codepostal:'75005'
+    }}
+    ,{firstname:'cnd1',lastname:'baadjis',address:{
+        rue:'18 rue liné',codepostal:'75005'
+    }}]
+    
+  it('should return true',()=>{
+      const res =readJson(__dirname+'/test2.json') as any[];
+      const datar=updateJsonWithArray(data,__dirname+'/test2.json',true);
+      //after update with many
+      const res1=readJson(__dirname+'/test2.json') as any[]
+
+      
+      // compare length
+      expect(res.length<res1.length).to.be.true
+  })
+
+})
