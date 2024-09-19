5f609462d7657d4c14a5f80ce1dbdb607dcba1e9
baadjis <baadjisidy@gmail.com>
2024-09-19 12:17:24 +0200

    changed option type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,18 +1,19 @@
 export type CompareFn<T>=(a:T,b:T)=>number;
+export type BooleanFn<T>=(a:T)=>boolean
 interface Dictionary<T> {
     [key:string]:T 
 }
 export type OptionType={
-    limit?:number,
-    skip?:number,
-    sort?:CompareFn<any>|Dictionary<number>
+    $limit?:number,
+    $skip?:number,
+    $sort?:CompareFn<any>|Dictionary<number>
 }
 
 export interface SchemaField<T>{
     type:Function,
     required?:boolean,
     default?:T
-    validator?:Function
+    validator?:BooleanFn<T>
 }
 export type DescriptorType={
     [key:string]:SchemaField<any>|DescriptorType
